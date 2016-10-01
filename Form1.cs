26bb7a89aae9e6ba7592b9709be5642601c8f9a2
Wesley Dettmer <w.dettmer@hotmail.com>
2016-10-01 18:55:02 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador1
+{
+    public partial class FalloutDrox : Form
+    {
+        public FalloutDrox()
+        {
+            InitializeComponent();
+            
+        }
+        //Botão "Voltar".
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+        // Botão "Avançar".
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+        // Botão "Atualizar".
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+        //Caixa de texto "URL".
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //Botão "Ir".
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+        }
+        //Botão "Home"/"Pagina principal".
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.google.com.br");
+        }
+    }
+}
